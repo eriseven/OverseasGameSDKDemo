742d73e9e875a9e6fe80293fdd8499bd26e2df2e
lidingyu <lidingyu@sohu.com>
2022-02-27 00:03:30 +0800

    add Sign In Interface

---
+++ Assets/OverseasGameSDKDemo/Scripts/GoogleSignInImp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoogleSignInImp : ISignInInterface
+{
+    public string SignInPlatform => "Google";
+    
+    public void TryQuickSignIn(Action<SignInResult> finished)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SignIn(Action<SignInResult> finished)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SignOut()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/OverseasGameSDKDemo/Scripts/SignInManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SignInResult
+{
+    public string Error;
+    public string SignInPlatform;
+    public string OpenID;
+    public string Token;
+}
+
+internal interface ISignInInterface
+{
+    string SignInPlatform { get; }
+    void TryQuickSignIn(Action<SignInResult> finished);
+    void SignIn(Action<SignInResult> finished);
+    void SignOut();
+}
+
+public class SignInManager
+{
+    static SignInManager()
+    {
+        
+    }
+    
+    public static void TryQuickSignIn(Action<SignInResult> finished)
+    {
+    }
+
+    public static void SignIn(string signInPlatform, Action<SignInResult> finished)
+    {
+    }
+
+    public static void SignOut()
+    {
+    }
+}
