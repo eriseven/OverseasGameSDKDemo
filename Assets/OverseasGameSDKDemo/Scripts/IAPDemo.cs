bd39e0f9f910844b88dcaedae31e347e1ca1f676
lidingyu <lidingyu@sohu.com>
2022-03-09 14:47:21 +0800

    add IAP test demo

---
+++ Assets/OverseasGameSDKDemo/Scripts/IAPDemo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+// using Product = Facebook.Unity.Product;
+
+public class IAPDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public int userCredits = 10;
+
+    public void GrantCredits(int credits)
+    {
+        userCredits = userCredits + credits;
+        Debug.Log("You received " + credits + " Credits!");
+    }
+
+    public void OnPurchaseFailed(Product i, PurchaseFailureReason p)
+    {
+        if (p == PurchaseFailureReason.PurchasingUnavailable)
+        {
+            // IAP may be disabled in device settings.
+        }
+        Debug.LogError(i.ToString() + p.ToString());
+    }
+}
